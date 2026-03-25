74ef59ea055cc2e7c26e6e6824a54ff6b52499f1
RyukyuSpirit <sdavis3491@gmail.com>
2026-03-24 22:38:11 -0500

    adds replication context

--- prox-tuah/ProxmoxHandler.py
+++ prox-tuah/ProxmoxHandler.py
@@ -112,6 +112,7 @@ class ProxmoxHandler(ProxmoxAPI):
             "pool": "name",
             "pools": "poolid",
             "qemu": "vmid",
+            "replication": "id",
             "rules": "pos",
             "services": "service",
             "snapshot": "snapname",
